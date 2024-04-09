55161cbdf0d5918b35aaf7151910648b791e7b1e
Hugo Sousa <hmosousa@gmail.com>
2024-04-09 17:00:59 +0100

    Refactored translation.py to take more arguments.

--- scripts/translate.py
+++ scripts/translate.py
@@ -9,37 +9,50 @@ from src.translator import Translator
 
 logging.basicConfig(level=logging.INFO)
 
-def main(lang="en"):
+def main(lang="en", name="pt_vid", domain="default", split="train"):
+    """
+    name = ["pt_vid", "frmt", "dsl_tl"]
+
+    Run the following commands to translate the datasets:
+
+    python scripts/translate.py -l "en" -n "pt_vid" -d "journalistic" -s "train"
+    python scripts/translate.py -l "en" -n "pt_vid" -d "legal" -s "train"
+    python scripts/translate.py -l "en" -n "pt_vid" -d "literature" -s "train"
+    python scripts/translate.py -l "en" -n "pt_vid" -d "politics" -s "train"
+    python scripts/translate.py -l "en" -n "pt_vid" -d "social_media" -s "train"
+    python scripts/translate.py -l "en" -n "pt_vid" -d "web" -s "train"
+    python scripts/translate.py -l "en" -n "dsl_tl" -d "default" -s "train"
+    python scripts/translate.py -l "en" -n "dsl_tl" -d "default" -s "test"
+    python scripts/translate.py -l "en" -n "frmt" -d "default" -s "train"
+    python scripts/translate.py -l "en" -n "frmt" -d "default" -s "test"
+    """
     translator = Translator(source="pt", target=lang)
 
     idx = 0
-    source_ds = TranslationDataset("pt")
-    datasets = ["pt_vid", "frmt", "dsl_tl"]  # TODO: add "pt_vid"
-    for name in datasets:
-        logging.info(f"Translating dataset {name}.")
-        dataset = load_dataset(name)
-        for domain in dataset:
-            for split in dataset[domain]:
-                for text in tqdm(dataset[domain][split]):
-                    if idx not in source_ds:
-                        try:
-                            translate = translator.translate(text)
-                            data = {
-                                "idx": idx,
-                                "source": name,
-                                "domain": domain,
-                                "split": split,
-                                "pt": text, 
-                                "en": translate
-                            }
-                            source_ds.add(idx, data)
-                        except Exception as e:
-                            print(f"Error translating {idx}: {e}")
-                    else:
-                        logging.info(f"Skipping {idx}.")
-                    idx += 1
-                    if idx % 100 == 0:
-                        source_ds.save()
+    
+    logging.info(f"Translating dataset {name}.")
+    dataset = load_dataset(name)
+    source_ds = TranslationDataset(f"{lang}_{name}_{domain}_{split}")
+    for text in tqdm(dataset[domain][split]):
+        if idx not in source_ds:
+            try:
+                translate = translator.translate(text)
+                data = {
+                    "idx": idx,
+                    "source": name,
+                    "domain": domain,
+                    "split": split,
+                    "pt": text, 
+                    "en": translate
+                }
+                source_ds.add(idx, data)
+            except Exception as e:
+                print(f"Error translating {idx}: {e}")
+        else:
+            logging.info(f"Skipping {idx}.")
+        idx += 1
+        if idx % 100 == 0:
+            source_ds.save()
     source_ds.save()
 
 
