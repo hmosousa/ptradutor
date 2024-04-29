94d76a023655b5184cba09d72d7490f838a17eb3
Hugo Sousa <hmosousa@gmail.com>
2024-04-29 12:48:31 +0100

    Had to chunck the train dataset to push to hf.

--- scripts/push2hf.py
+++ scripts/push2hf.py
@@ -8,6 +8,8 @@ from src.constants import DATA_PATH
 HF_PATH = DATA_PATH / "hf"
 HF_PATH.mkdir(exist_ok=True)
 
+N_CHUNKS = 5
+
 logging.basicConfig(level=logging.INFO)
 
 train, test = [], []
@@ -23,10 +25,20 @@ for filepath in DATA_PATH.glob("*.json"):
             test.append(info)
 
 train_path = HF_PATH / "train.jsonl"
-json.dump(train, train_path.open("w"))
+n_train = len(train)
+logging.debug(f"Train size: {n_train}")
+train_fps = []
+for chunk in range(N_CHUNKS):
+    start = chunk * n_train // N_CHUNKS
+    end = (chunk + 1) * n_train // N_CHUNKS
+    logging.debug(f"Chunk {chunk}: {start} - {end}")
+    chunk_path = HF_PATH / f"train_{chunk}.jsonl"
+    json.dump(train[start:end], chunk_path.open("w"))
+    train_fps.append(str(chunk_path))
+
 test_path = HF_PATH / "test.jsonl"
 json.dump(test, test_path.open("w"))
 
-data_files = {"train": str(train_path), "test": str(test_path)}
+data_files = {"train": train_fps, "test": str(test_path)}
 dataset = datasets.load_dataset("json", data_files=data_files)
 dataset.push_to_hub("liaad/PTradutor")
